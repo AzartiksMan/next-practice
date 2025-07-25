ee75a804afe8b2e5f01e0f3654c0d54782c8c988
AzartiksMan <azartofgod@gmail.com>
2025-07-25 14:05:33 +0300

    fix delete route1

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,10 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function DELETE(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function DELETE(_, { params }) {
   const { id } = params;
 
   if (!id) {
